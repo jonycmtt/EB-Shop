3a4ac54b813a12c68bd12ddc3e83eea2a64edbd8
jony <jonyislamcmt@gmail.com>
2023-03-09 17:42:43 +0600

    add javascript

--- js/script.js
+++ js/script.js
@@ -1,5 +1,38 @@
 let searchForm = document.querySelector('.search-form');
+let shoppingCart = document.querySelector('.shopping-cart');
+let logInForm = document.querySelector('.login-form');
+let navbar = document.querySelector('.navbar')
 
 document.querySelector('#search-btn').addEventListener('click', () => {
     searchForm.classList.toggle('active')
+    shoppingCart.classList.remove('active')
+    logInForm.classList.remove('active')
+    navbar.classList.remove('active')
 })
+document.querySelector('#cart-btn').addEventListener('click', () => {
+    shoppingCart.classList.toggle('active')
+    searchForm.classList.remove('active')
+    logInForm.classList.remove('active')
+    navbar.classList.remove('active')
+
+})
+document.querySelector('#login-btn').addEventListener('click', () => {
+    logInForm.classList.toggle('active')
+    searchForm.classList.remove('active')
+    shoppingCart.classList.remove('active')
+    navbar.classList.remove('active')
+})
+
+document.querySelector('#menu-btn').onclick = () => {
+    navbar.classList.toggle('active')
+    searchForm.classList.remove('active')
+    shoppingCart.classList.remove('active')
+    logInForm.classList.remove('active')
+    
+}
+window.onscroll = ()=> {
+    searchForm.classList.remove('active')
+    shoppingCart.classList.remove('active')
+    logInForm.classList.remove('active')
+    navbar.classList.remove('active')
+}
